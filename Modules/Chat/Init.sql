1586e08eebcca4a56ff025bbba025ed47d6a6f81
Kyllian Perrin <kyllian.perrin@gmail.com>
2017-04-24 10:01:46 +0200

    Corrected chat/init.sql
    A small correction on the "foreign" word

--- Modules/Chat/Init.sql
+++ Modules/Chat/Init.sql
@@ -8,5 +8,5 @@ idsalon number(6),
 iduser number(6),
 constraint pk_droit_salons primary key (idsalon, iduser),
 constraint fk_droit_salons_salon foreign key (idsalon) references salons(idsalon),
-constraint fk_droit_salons_user forgeign key (iduser) references user(iduser)
+constraint fk_droit_salons_user foreign key (iduser) references user(iduser)
 )
