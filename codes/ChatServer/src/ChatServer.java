2d966b10b04e0f1d47b526f8685a43c5f01e9832
carineamin <carine.amin@etu.univ-grenoble-alpes.fr>
2017-04-25 15:24:47 +0200

    chargement et ajout de profil

--- codes/ChatServer/src/ChatServer.java
+++ codes/ChatServer/src/ChatServer.java
@@ -1,4 +1,3 @@
-
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
@@ -31,32 +30,72 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 
 	}
 
-	public boolean login(ChatClientInt a) throws RemoteException {
-		//System.out.println(a.getName() + " essaye de se connecter ....");
-		int resultat = 1;
-		ResultSet rset;
-		try {
-			rset = stmt.executeQuery(
-					"select 'la date d''aujourd''hui est '||" + "to_char (sysdate, 'DD/MM/YYYY')" + "from dual");
-			while (rset.next()) {
-				System.out.println(rset.getString(1));
-			}
-			rset = stmt.executeQuery("Select idUser from Utilisateur where username='" + a.getName() + "'");
-			while (rset.next()) {System.out.println(rset.getString(1));
-				//int id = Integer.valueOf(rset.getString(1));
-				//System.out.println(id);
-				a.tell("Connexion accepté.");
-				// Carine devrat récup l'id client en chargeant le fichier
-				//publish(a.getName() + " est connecté.", id);
-				v.add(a);
-				return true;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		public boolean login(ChatClientInt a) throws RemoteException, SQLException{	
+		//System.out.println(a.getName() + "  got connected....");	
+		////////////////////////////////////////
+         Connection conn;
+         Statement stmt;
+         ResultSet rset;
+         int resultat = 1;
 
-		return false;
-	}
+         /* System.out.println("Veuillez saisir un login :");
+          username = sc.nextLine();
+          System.out.println("Veuillez saisir un mot de passe :");
+          password = sc.nextLine();*/
+         try {
+			Class.forName(jdbcDriver);
+		
+	         conn = DriverManager.getConnection(dbUrl,login, mdp);
+	         stmt = conn.createStatement();
+	         rset = stmt.executeQuery(
+	                 "select 'la date d''aujourd''hui est '||"
+	                 + "to_char (sysdate, 'DD/MM/YYYY')"
+	                 + "from dual");
+	         while (rset.next()) {
+	             System.out.println(rset.getString(1));
+	         }        
+	         } catch (ClassNotFoundException | SQLException e) {
+	      		// TODO Auto-generated catch block
+	      		e.printStackTrace();
+	      	}
+	         //recupère l'idUser
+	         int idUser = Utilisateur.rechercheUtilisateur(a.getName());
+	         if (idUser != 0){
+	        	 Utilisateur.chargerProfil(idUser);
+	         }
+	         else{
+	        	 System.out.println("Vous n'avez pas de profil, voulez vous créer un? (O ou N)");
+	        	 Scanner sc = new Scanner(System.in);
+	        	 String Rep = sc.nextLine();
+	        	 if (Rep.compareTo("O") == 0){
+	        		 System.out.println("Veuillez choisir un username");
+	        		 String username = sc.nextLine();
+	        		 System.out.println("Veuillez choisir un mot de passe");
+	        		 String mdp = sc.nextLine();
+	        		 idUser = Utilisateur.calcul_idUser();
+	        		 Utilisateur.ajouterUtilisateur(idUser, username, mdp);
+	        		 
+	        		 System.out.println("Veuillez entrer votre nom");
+	        		 String nom = sc.nextLine();
+	        		 System.out.println("Veuillez entrer votre prénom");
+	        		 String prenom = sc.nextLine();	        		 
+	        		 Utilisateur.ajouterProfil(idUser, nom, prenom);
+	        		 
+	        	 }else{
+	        		 System.out.println("Connexion impossible");
+	        		 return false;
+	        	 }
+	        	 
+ 
+	         }
+ 
+		
+  	////////////////////////////////////////
+  	a.tell("You have Connected successfully.");
+	publish(a.getName()+ " has just connected.",idUser);
+  	v.add(a);
+	return true;
+  	}	
 
 	// Le serveur recoit un message d'un client
 	public void publish(String s, int id) throws RemoteException {
@@ -85,5 +124,5 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 	public Vector getConnected() throws RemoteException {
 		return v;
 	}
-
 }
+	
