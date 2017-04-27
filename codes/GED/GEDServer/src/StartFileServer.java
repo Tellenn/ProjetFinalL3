66db1d8f0be783fd3352c1c8f324940810b94b63
Kyllian Perrin <kyllian.perrin@gmail.com>
2017-04-27 14:45:22 +0200

    Update StartFileServer.java

--- codes/GED/GEDServer/src/StartFileServer.java
+++ codes/GED/GEDServer/src/StartFileServer.java
@@ -3,16 +3,19 @@ import java.rmi.Naming;
 public class StartFileServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String entete = new String("/home/p/perrink/Bureau/");
 		try{
 			
 			java.rmi.registry.LocateRegistry.createRegistry(1099);
 			
 			FileServer fs=new FileServer();
-                        //Nom du fichier a transferer
-			fs.setFile("/home/p/perrink/Bureau/me.jpg");	
+                        //Nom du fichier a transferer /!\ Crash si incorrect
+			fs.setFile(args[0],entete);	
                         //Adresse IP
-			Naming.rebind("rmi://152.77.82.229/abc", fs);
-			System.out.println("File Server is Ready");
+			
+	
+			Naming.rebind("rmi://152.77.82.210/abc", fs);
+			System.out.println("Server will transfer "+args[0]+" to any client");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
