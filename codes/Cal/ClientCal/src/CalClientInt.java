6b7f5cebb285ff8378e795eb3d7e61c254c4b076
blondelq <blondelq@hotmail.fr>
2017-04-27 13:25:36 +0200

    Revert "add Cal"
    This reverts commit c5431562b44b0a6f1460509d9e5dbc5740b017be.

--- codes/Cal/ClientCal/src/CalClientInt.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.Date;
- 
-public interface CalClientInt extends Remote{	
-	public void createEventOrder(String libelle, Date dateDebut, Date dateFin) throws RemoteException;
-}
