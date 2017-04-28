bd98c383f39cbde34ed0150ab247c0d9614b8887
Kyllian Perrin <kyllian.perrin@etu.univ-grenoble-alpes.fr>
2017-04-28 11:56:15 +0200

    Making changes in the GED

--- codes/GED/GEDServer/src/FileClientInt.java
+++ codes/GED/GEDServer/src/FileClientInt.java
@@ -3,7 +3,9 @@ import java.rmi.RemoteException;
  
 public interface FileClientInt extends Remote{
  
-	public boolean sendData(String filename, byte[] data, int len) throws RemoteException;
+	public boolean receiveData(String filename, byte[] data, int len) throws RemoteException;
+	
+	public boolean sendData() throws RemoteException;
 	
 	public String getName() throws RemoteException;
 }
--- codes/GED/GEDServer/src/FileServerInt.java
+++ codes/GED/GEDServer/src/FileServerInt.java
@@ -2,5 +2,9 @@ import java.rmi.*;
  
 public interface FileServerInt extends Remote{
  
-		public boolean login(FileClientInt c) throws RemoteException;
+		public boolean login(FileClientInt c, int order) throws RemoteException;
+		
+		public boolean sendData(FileClientInt c) throws RemoteException;
+		
+		public boolean receiveData(String filename, byte[] data, int len) throws RemoteException;
 }
