a1af162af4084af790b29587efdd92085d19805a
carineamin <carine.amin@etu.univ-grenoble-alpes.fr>
2017-05-02 10:27:18 +0200

    Delete UserClientInt.java

--- codes/Users/ChatClientProj/src/UserClientInt.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
- 
-public interface UserClientInt extends Remote{	
-	public void tell (String name)throws RemoteException ;
-	public String getName()throws RemoteException ;
-}
