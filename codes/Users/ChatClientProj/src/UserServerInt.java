98b18fb9efcb936279cf33eb664e824736a6fb46
carineamin <carine.amin@etu.univ-grenoble-alpes.fr>
2017-05-02 10:28:08 +0200

    Delete UserServerInt.java

--- codes/Users/ChatClientProj/src/UserServerInt.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-import java.util.*;
- 
-public interface UserServerInt extends Remote{	
-	public boolean login (UserClientInt a)throws RemoteException ;
-	public void publish (String s, int id )throws RemoteException ;
-	public Vector getConnected() throws RemoteException ;
-}
