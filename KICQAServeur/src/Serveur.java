d73bc35fc191407545746571129852b1359f7456
Kyllian Perrin <kyllian.perrin@gmail.com>
2017-05-03 16:39:53 +0200

    Update Serveur.java

--- KICQAServeur/src/Serveur.java
+++ KICQAServeur/src/Serveur.java
@@ -1,20 +1,18 @@
 import java.rmi.Naming;
 
 public class Serveur {
-	static String ip;
-	
-	public void Serveur(String ip){
-		Serveur.ip=ip;
-	}
 
 	/**
 	 * Permet de demarer le serveur GED
+	 * @param ip l'ip du serveur
 	 */
-	public static void startGEDserver() {
-		System.out.println("Demarage du serveur GED");
+	public static void startGEDserver(String ip) {
+		System.out.println("Demarrage du serveur GED");
 		try{
-			java.rmi.registry.LocateRegistry.createRegistry(2220);
+			java.rmi.registry.LocateRegistry.createRegistry(1099);
+			System.out.println("Port ok");
 			GEDServeur fs=new GEDServeur();
+			System.out.println("New server ok");
 			Naming.rebind("rmi://"+ip+"/abc", fs);
 			System.out.println("Le serveur GED est pret a opérer");
 		}catch(Exception e){
@@ -24,11 +22,12 @@ public class Serveur {
 	
 	/**
 	 * Permet de demarer le serveur Calendrier
+	 * @param ip l'ip du serveur
 	 */
-	public static void startCALserver() {
-		System.out.println("Demarage du serveur CAL");
+	public static void startCALserver(String ip) {
+		System.out.println("Demarrage du serveur CAL");
 		try{
-			java.rmi.registry.LocateRegistry.createRegistry(2221);
+			java.rmi.registry.LocateRegistry.createRegistry(1100);
 			CalServer fs=new CalServer();
 			Naming.rebind("rmi://"+ip+"/abc", fs);
 			System.out.println("Le serveur CAL est pret a opérer");
@@ -39,11 +38,12 @@ public class Serveur {
 	
 	/**
 	 * Permet de demarer le serveur Gestion utilisateurs
+	 * @param ip l'ip du serveur
 	 */
-	public static void startUSERserver() {
-		System.out.println("Demarage du serveur USER");
+	public static void startUSERserver(String ip) {
+		System.out.println("Demarrage du serveur USER");
 		try{
-			java.rmi.registry.LocateRegistry.createRegistry(2222);
+			java.rmi.registry.LocateRegistry.createRegistry(1101);
 			UserServer fs=new UserServer();
 			Naming.rebind("rmi://"+ip+"/abc", fs);
 			System.out.println("Le serveur USER est pret a opérer");
@@ -54,11 +54,12 @@ public class Serveur {
 	
 	/**
 	 * Permet de demarer le serveur Chat
+	 * @param ip l'ip du serveur
 	 */
-	public static void startCHATserver() {
-		System.out.println("Demarage du serveur CHAT");
+	public static void startCHATserver(String ip) {
+		System.out.println("Demarrage du serveur CHAT");
 		try{
-			java.rmi.registry.LocateRegistry.createRegistry(2223);
+			java.rmi.registry.LocateRegistry.createRegistry(1102);
 			ChatServer fs=new ChatServer();
 			Naming.rebind("rmi://"+ip+"/abc", fs);
 			System.out.println("Le serveur CHAT est pret a opérer");
