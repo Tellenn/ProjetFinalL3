4995c34d9204d1e589b005d14aa4926d22cf16dc
Kyllian Perrin <kyllian.perrin@gmail.com>
2017-05-03 09:43:39 +0200

    Update FileClientInt.java

--- codes/GED/GEDServer/src/FileClientInt.java
+++ codes/GED/GEDServer/src/FileClientInt.java
@@ -1,11 +1,11 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
- 
-public interface FileClientInt extends Remote{
- 
+
+public interface FileClientInt extends Remote {
+
 	public boolean receiveData(String filename, byte[] data, int len, String cible) throws RemoteException;
-	
-	public boolean sendData(FileServerInt server, String path, String file) throws RemoteException;
-	
+
+	public int sendData(FileServerInt server, String path, String file) throws RemoteException;
+
 	public String getName() throws RemoteException;
 }
