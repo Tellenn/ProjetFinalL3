2d44429a51c50d0f537e6df9d97ff98313af26d5
NtnChrrn <antoine.charron@etu.univ-grenoble-alpes.fr>
2017-05-04 02:40:31 +0200

    Update ChatServer.java

--- KICQAServeur/src/ChatServer.java
+++ KICQAServeur/src/ChatServer.java
@@ -14,7 +14,7 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 	private TreeMap<Integer, ChatClientInt> p = new TreeMap<Integer, ChatClientInt>();
 
 	private static final String dbUrl 		= "jdbc:oracle:thin:@im2ag-oracle.e.ujf-grenoble.fr:1521:im2ag";
-	private static final String jdbcDriver 	= "oracle.jdbc.driver.OracleDriver";
+	private static final String jdbcDriver 		= "oracle.jdbc.driver.OracleDriver";
 	private static final String login 		= "charroan";
 	private static final String mdp 		= "Aclf2016";
 	private Statement 	 stmt;
@@ -33,6 +33,13 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 
 	}
 
+	/** authentifie le client sur le serveur
+	 * @param a : client
+	 * @param login : login
+	 * @param mdp : mot de passe
+	 * @return idClient
+	 * @throws RemoteException: s'il y un problème de communication
+	 */
 	public int login(ChatClientInt a, String login, String mdp) throws RemoteException {
 		//System.out.println(a.getName() + " essaye de se connecter ....");
 		ResultSet rset;
@@ -42,11 +49,11 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 			while (rset.next()) {
 				System.out.println(rset.getString(1));
 			}
-			// Vérifie les données de connexion
+			// VÃ©rifie les donnÃ©es de connexion
 			rset = stmt.executeQuery("Select idUser from Utilisateur where username='" + login + "' AND userPassword='"+mdp +"'");
 			while (rset.next()) {
-				System.out.println("Connexion accepté pour "+rset.getString(1)+ "id "+a.getId());
-				a.tell("Connexion acceptée. ");
+				System.out.println("Connexion acceptÃ© pour "+rset.getString(1)+ "id "+a.getId());
+				a.tell("Connexion acceptÃ©e. ");
 				a.setId(Integer.valueOf(rset.getString(1)));
 				p.put(a.getId(), a);
 				return Integer.valueOf(rset.getString(1));
@@ -58,59 +65,54 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 		return -1;
 	}
 
-	// Le serveur recoit un message d'un client et le transmet à tous les utilisateurs connectés
-	public void publish(String s, int id) throws RemoteException {
-		// TODO: A reprendre
-		// *** On envoie à tous les utilisateurs conecté le message
-		for (ChatClientInt item : p.values()) {
-			try {		
-				item.tell(s);
-			} catch (Exception e) {
-				// problem with the client not connected.
-				// Better to remove it
-			}
-		}
-		// *** On met à jour la conversation
-		// !!!!! Mettre le bon id2
-		ConversationPrivee c = new ConversationPrivee(id,3);
-		c.ajouterMessage(id, s);
-	}
+	
 	
 	// Conversation privee
-	// Le serveur recoit un message d'un client et le transmet à id2
+	// Le serveur recoit un message d'un client et le transmet Ã  id2
 	public void publishPrivate(String s, int id1, int id2 ) throws RemoteException {
-		// *** On envoie à l'utilisateur conecté le message	
-		//System.out.println(p.get(id1).getName());			
+		// *** On envoie Ã  l'utilisateur conectÃ© le message		
 		
-		System.out.println("[System]publishPrivate(id1:"+id1+" id2:"+id2+")");
-		// *** On met à jour la conversation
+		//System.out.println("[System]publishPrivate(id1:"+id1+" id2:"+id2+")");
+		// *** On met Ã  jour la conversation
 		ConversationPrivee c = new ConversationPrivee(id1,id2);
 		c.ajouterMessage(id1, s);	
 		TreeMap<String, String> t =new TreeMap<String, String>(c.getLastMessage());
 		t.put("nom", p.get(Integer.valueOf(t.get("emetteur"))).getName());
 		try {
-			//si l'utilisateur est connecté, il sera avertit			
+			//si l'utilisateur est connectÃ©, il sera avertit			
 			p.get(id1).tell(t);				
 		}catch (Exception e) {
-			System.out.println("[System]ERREUR: client non connecté");
-			// sinon l'utilisateur n'est pas connecté
+			System.out.println("[System]ERREUR: client non connectÃ©");
+			// sinon l'utilisateur n'est pas connectÃ©
 		}
 		try {
-			//si l'utilisateur est connecté, il sera avertit
+			//si l'utilisateur est connectÃ©, il sera avertit
 			if(id1!=id2)p.get(id2).tell(t);				
 		}catch (Exception e) {
-			// sinon l'utilisateur n'est pas connecté, on rattrape l'erreur et on fait rien		
+			// sinon l'utilisateur n'est pas connectÃ©, on rattrape l'erreur et on fait rien		
 		}
 	}
 
+	/** Permet de récupérer les clients connectés
+	 * @return les clients connectes
+	 * @throws RemoteException: s'il y un problème de communication
+	 */
 	public TreeMap<Integer, ChatClientInt> getConnected() throws RemoteException {
 		return p;
 	}
 
+	/** Récupère le client avec l'id id
+	 * @param i : id du client
+	 * @return le client d'id id
+	 * @throws RemoteException: s'il y un problème de communication
+	 */
 	public ChatClientInt getClient(int id) throws RemoteException{
 		return (ChatClientInt)p.get(id);
 	}
 	
+	/** Affiche les idClients des clients connectés du serveur
+	 * @throws RemoteException
+	 */
 	public void afficherIdClients() throws RemoteException{
 		for (ChatClientInt item : p.values()) {
 			try {		
@@ -122,6 +124,11 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 		}
 	}
 	
+	/** Récupère la totalité des messages et met à jour le client
+	 * @param a: le client
+	 * @param idPersonne2: le recepteur
+	 * @throws RemoteException
+	 */
 	public void uploadConversation(ChatClientInt a, int idPersonne2)throws RemoteException{
 		ConversationPrivee c = new ConversationPrivee(a.getId(),idPersonne2);
 		List<TreeMap<String, String>> tt =new ArrayList<TreeMap<String, String>>(c.getMessages());
@@ -132,23 +139,21 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
 		}
 	}
 	
+	/** Récupère la totalité des salons concernés par le client a
+	 * @param a : client concerné
+	 * @throws RemoteException
+	 */
 	public void uploadSalon	(ChatClientInt a) throws RemoteException {
 		ResultSet rset;
-		String NameSalon; 
-		TreeMap<Integer, String> tt =new TreeMap<Integer, String>();
 			try {
 				rset = stmt.executeQuery("Select IdSalon from droitUtilisateur where idUser=" + a.getId());	
 			
-				while (rset.next()) {
-					System.out.println("Salon "+rset.getString(1)+ " avec iduser= "+a.getId());
+				while (rset.next()) {					
 					ConversationSalon c = new ConversationSalon(a.getId());
 					a.addSalon(Integer.valueOf(rset.getString(1)), c.getNameSalon());				
 				}
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	}
-
-	
 }
